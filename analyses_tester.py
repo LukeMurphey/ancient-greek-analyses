74bda27d0cddf21473c6933bc06f1e3e5f4cc781
Luke Murphey <Luke.Murphey@gmail.com>
2022-03-24 21:46:06 -0700

    Added detection of words with meaning that is too short

--- analyses_tester.py
+++ analyses_tester.py
@@ -7,6 +7,7 @@ class GreekAnalysesTester(GreekAnalysesParser):
         self.results = {
             "missing_meaning": 0,
             "has_strange_meaning": 0,
+            "has_short_meaning": 0,
         }
 
     def is_missing_meaning(self, form, lemma_form, meaning, details, attrs, definition):
@@ -14,6 +15,9 @@ class GreekAnalysesTester(GreekAnalysesParser):
 
     def has_strange_meaning(self, form, lemma_form, meaning, details, attrs, definition):
         return re.search('\[|,\]', meaning) != None
+
+    def has_short_meaning(self, form, lemma_form, meaning, details, attrs, definition):
+        return len(meaning.strip()) <= 1 and len(meaning.strip()) != 0
 
     def process_word_description(self, form, lemma_form, meaning, details, attrs, definition):
 
@@ -25,8 +29,13 @@ class GreekAnalysesTester(GreekAnalysesParser):
         if self.has_strange_meaning(form, lemma_form, meaning, details, attrs, definition):
             self.results["has_strange_meaning"] += 1
 
+        # Detect meanings that appear to be too short
+        if self.has_short_meaning(form, lemma_form, meaning, details, attrs, definition):
+            self.results["has_short_meaning"] += 1
+
         return None
 
     def print_results(self):
         print("Missing meanings: " + str(self.results["missing_meaning"]))
         print("Has strange meanings: " + str(self.results["has_strange_meaning"]))
+        print("Has short meanings: " + str(self.results["has_short_meaning"]))
